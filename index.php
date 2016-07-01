606f04f95392ccea0b67c89ab4f81a45427ddcfb
Nicholas Koskowski <Nickkoskowski@gmail.com>
2016-07-01 15:50:17 -0700

    Fixed a merge conflict

--- index.php
+++ index.php
@@ -15,48 +15,6 @@
 						<?php echo '<img src="'.get_field('type_image', $terms).'">'; ?>
 			<a class="btn btn-block"><i class="fa fa-exclamation-circle" aria-hidden="true"></i> Watch a 2 minute video -  "How to use this library"</a>
 		</div>
-		<h4 class="text-center">Search by Types</h4>
-		<div class="row">
-			<div class="col-md-3 col-xs-4 type">
-				<a href="<?php echo get_site_url(); ?>/type/diesel/">
-					<img src="<?php echo get_template_directory_uri(); ?>/images/deisel-icon.jpg">
-				</a>
-			</div>
-			<div class="col-md-3 col-xs-4 type">
-				<a href="<?php echo get_site_url(); ?>/type/class-a/">
-					<img src="<?php echo get_template_directory_uri(); ?>/images/class-a-icon.jpg">
-				</a>
-			</div>
-			<div class="col-md-3 col-xs-4 type">
-				<a href="<?php echo get_site_url(); ?>/type/class-b/">
-					<img src="<?php echo get_template_directory_uri(); ?>/images/class-b-icon.jpg">
-				</a>
-			</div>
-			<div class="col-md-3 col-xs-4 type">
-				<a href="<?php echo get_site_url(); ?>/type/class-c/">
-					<img src="<?php echo get_template_directory_uri(); ?>/images/class-c-icon.jpg">
-				</a>
-			</div>
-			<div class="col-md-3 col-xs-4 type">
-				<a href="<?php echo get_site_url(); ?>/type/travel-trailer/">
-					<img src="<?php echo get_template_directory_uri(); ?>/images/travel-trailer-icon.jpg">
-				</a>
-			</div>
-			<div class="col-md-3 col-xs-4 type">
-				<a href="<?php echo get_site_url(); ?>/type/5th-wheel/">
-					<img src="<?php echo get_template_directory_uri(); ?>/images/5thwheel.jpg">
-				</a>
-			</div>
-			<div class="col-md-3 col-xs-4 type">
-				<a href="<?php echo get_site_url(); ?>/type/tent-trailer/">
-					<img src="<?php echo get_template_directory_uri(); ?>/images/tent-trailer-icon.jpg">
-				</a>
-			</div>
-			<div class="col-md-3 col-xs-4 type">
-				<a href="<?php echo get_site_url(); ?>/type/toy-hauler/">
-					<img src="<?php echo get_template_directory_uri(); ?>/images/toyhauler-icon.jpg">
-				</a>
-			</div>
 		<?php endforeach; ?>
 		</div>
 		<h4 class="text-center">Search by Manufacturer</h4>
