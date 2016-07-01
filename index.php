9d3f98f3c4a796448f8ddd9bc3b9252b50defd27
Nicholas Koskowski <Nickkoskowski@gmail.com>
2016-07-01 15:49:28 -0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 <main>
 	<div class="container">
 		<div class="text-center">
-<<<<<<< HEAD
 			<a class="btn" href="/instructions"><i class="fa fa-exclamation-circle" aria-hidden="true"></i> Watch a 2 minute video -  "How to use this library"</a>
 		</div>
 		<h4 class="text-center">Search by Types</h4>
@@ -14,7 +13,6 @@
 			<div class="col-md-3 col-xs-6 type" style="text-align:center;">
 				<a href="<?php echo get_term_link($type); ?>">
 						<?php echo '<img src="'.get_field('type_image', $terms).'">'; ?>
-=======
 			<a class="btn btn-block"><i class="fa fa-exclamation-circle" aria-hidden="true"></i> Watch a 2 minute video -  "How to use this library"</a>
 		</div>
 		<h4 class="text-center">Search by Types</h4>
@@ -64,19 +62,16 @@
 		</div>
 		<h4 class="text-center">Search by Manufacturer</h4>
 		<div class="row">
-<<<<<<< HEAD
 			<?php $brands = get_terms( 'brands', array('orderby' => 'title','hide_empty' => 1) );
 			foreach($brands as $brand) :
 				$terms = get_term($brand->term_id, 'brands');
 				?>
 				<div class="col-xs-6 col-md-2 brand">
-=======
 			<?php $brands = get_terms( 'brands', array('orderby' => 'count','hide_empty' => 1) );
 			foreach($brands as $brand) :
 				$terms = get_term($brand->term_id, 'brands');
 				?>
 				<div class="col-xs-6 col-md-3 brand">
->>>>>>> origin/master
 					<a href="<?php echo get_term_link($brand); ?>">
 						<?php echo '<img src="'.get_field('logo', $terms).'">'; ?>
 					</a>
